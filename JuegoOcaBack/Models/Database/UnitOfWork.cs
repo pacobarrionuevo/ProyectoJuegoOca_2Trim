b95700d1ece1f5ecc9eae4fbca18eaed36c76642
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-01-14 20:56:16 +0100

    UnitOfWork added

--- JuegoOcaBack/Models/Database/UnitOfWork.cs
+++ JuegoOcaBack/Models/Database/UnitOfWork.cs
@@ -2,5 +2,24 @@
 {
     public class UnitOfWork
     {
+        //El unitofwork es muy parecido a lo que tiene Jose (evidentemente adaptado a lo nuestro)
+        private readonly ProyectoDbContext _context;
+
+        //Todas los repositorios de los productos que queremos guardar
+
+
+        // Exponer el DbContext
+        public ProyectoDbContext Context => _context;
+
+        public UnitOfWork(ProyectoDbContext context)
+        {
+            _context = context;
+        }
+
+        //Método para guardar
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
     }
 }
