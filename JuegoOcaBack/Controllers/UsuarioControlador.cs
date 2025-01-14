98ed2b11a69428d2ee58812c10e505626dd2d89f
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-01-14 21:15:09 +0100

    PasswdHelped, Empiezo el controlador usuario y dependecias añadidas

--- JuegoOcaBack/Controllers/UsuarioControlador.cs
+++ JuegoOcaBack/Controllers/UsuarioControlador.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JuegoOcaBack.Models.Database;
+using JuegoOcaBack.Recursos;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JuegoOcaBack.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UsuarioControlador : ControllerBase
-    {
+    {   
+        private readonly ProyectoDbContext _context;
+        private readonly PasswordHelper passwordHelper;
+        private readonly TokenValidationParameters _tokenParameters;
+
+        public UsuarioControlador(ProyectoDbContext _dbContext, IOptionsMonitor<JwtBearerOptions> jwtOptions)
+        {
+            _context = _dbContext;
+            _tokenParameters = jwtOptions.Get(JwtBearerDefaults.AuthenticationScheme).TokenValidationParameters;
+        }
     }
 }
---
+++ JuegoOcaBack/Recursos/PasswordHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace JuegoOcaBack.Recursos
+{
+    public class PasswordHelper
+    {
+        // Made by Jose aka nuestro profe favorito
+        public static string Hash(string password)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+            byte[] inputHash = SHA256.HashData(inputBytes);
+            return Encoding.UTF8.GetString(inputHash);
+        }
+    }
+}
