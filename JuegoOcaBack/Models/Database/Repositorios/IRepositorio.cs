b95de630589dda15549081af87f875abb0dfe154
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-01-14 20:26:26 +0100

    IRepositorio de Jose

--- JuegoOcaBack/Models/Database/Repositorios/IRepositorio.cs
+++ JuegoOcaBack/Models/Database/Repositorios/IRepositorio.cs
@@ -1,6 +1,15 @@
 ﻿namespace JuegoOcaBack.Models.Database.Repositorios
 {
-    public class IRepositorio
+    public interface IRepositorio<TEntity> where TEntity : class
     {
+        //Calcadisimo de los ejemplos de Jose
+        Task<ICollection<TEntity>> GetAllAsync();
+        IQueryable<TEntity> GetQueryable(bool asNoTracking = true);
+        Task<TEntity> GetByIdAsync(object id);
+        Task<TEntity> InsertAsync(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<bool> ExistsAsync(object id);
     }
+
 }
