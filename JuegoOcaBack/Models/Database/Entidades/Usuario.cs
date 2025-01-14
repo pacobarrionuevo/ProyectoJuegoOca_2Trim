fd29a68fe2ef3e862be0ad211c5e6bc6781a21ae
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-01-14 20:22:45 +0100

    Clase Usuario atributos

--- JuegoOcaBack/Models/Database/Entidades/Usuario.cs
+++ JuegoOcaBack/Models/Database/Entidades/Usuario.cs
@@ -2,5 +2,12 @@
 {
     public class Usuario
     {
+        public int UsuarioId { get; set; }
+        public string UsuarioFotoPerfil { get; set; } //Ruta de la imagen
+        public string UsuarioApodo { get; set; }
+        public string UsuarioEmail { get; set; }
+        public string UsuarioContrasena { get; set; }
+        public string UsuarioConfirmarContrasena { get; set; }
+
     }
 }
