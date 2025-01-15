152c515f3b959cf8ac439e49d5c29b902e581749
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-01-15 19:34:32 +0100

    approutes para el login component_ cambiar htmlcss y fondo

--- JuegoOcaFront/src/app/app.routes.ts
+++ JuegoOcaFront/src/app/app.routes.ts
@@ -1,4 +1,20 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [{ path: '', component: MainComponent }];
+export const routes: Routes = 
+[
+    { 
+        path: '', 
+        component: MainComponent 
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+];
