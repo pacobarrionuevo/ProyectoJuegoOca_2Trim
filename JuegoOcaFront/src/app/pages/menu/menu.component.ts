f80c425da3cb7fde73a48541171b3b2c6de316e6
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-01-29 19:26:28 +0100

    interfaz de el menu hecha.

--- JuegoOcaFront/src/app/pages/menu/menu.component.ts
+++ JuegoOcaFront/src/app/pages/menu/menu.component.ts
@@ -8,5 +8,58 @@ import { RouterModule } from '@angular/router';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+ 
+  activeSection: string = 'amigos';
 
+ 
+  amigos = [
+    { nombre: 'Amigo 1' },
+    { nombre: 'Amigo 2' },
+    { nombre: 'Amigo 3' }
+  ];
+
+
+  solicitudesPendientes = [
+    { nombre: 'Usuario 1' },
+    { nombre: 'Usuario 2' }
+  ];
+
+
+  cambiarSeccion(seccion: string) {
+    this.activeSection = seccion;
+  }
+
+  
+  eliminarAmigo(amigo: any) {
+    if (confirm(`¿Estás seguro de que quieres eliminar a ${amigo.nombre}?`)) {
+      this.amigos = this.amigos.filter(a => a !== amigo);
+    }
+  }
+
+
+  aceptarSolicitud(solicitud: any) {
+    this.amigos.push(solicitud); 
+    this.solicitudesPendientes = this.solicitudesPendientes.filter(s => s !== solicitud); // Eliminar de las solicitudes
+    alert(`${solicitud.nombre} ha sido añadido a tus amigos.`);
+  }
+
+
+  rechazarSolicitud(solicitud: any) {
+    this.solicitudesPendientes = this.solicitudesPendientes.filter(s => s !== solicitud); // Eliminar de las solicitudes
+    alert(`Solicitud de ${solicitud.nombre} rechazada.`);
+  }
+
+ 
+  cerrarSesion() {
+   
+    console.log('Sesión cerrada');
+    alert('Has cerrado sesión.');
+  }
+
+
+  irAEmparejamiento() {
+
+    console.log('Navegando a Emparejamiento');
+    alert('Redirigiendo a la vista de Emparejamiento...');
+  }
 }
