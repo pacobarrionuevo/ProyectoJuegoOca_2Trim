dc63ad4d00547b641a360d1e338fa4c0d0edd24e
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-01-29 15:39:42 +0100

    smartsearch backend donette

---
+++ JuegoOcaBack/Controllers/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using JuegoOcaBack.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JuegoOcaBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private readonly SmartSearchService _smartSearchService;
+        public MenuController( SmartSearchService smartSearchService)
+        {
+            _smartSearchService = smartSearchService;
+        }
+    }
+    
+}
