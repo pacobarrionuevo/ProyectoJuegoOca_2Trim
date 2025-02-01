4d3a20c15d2c6c49bb95cd1de1b65e2b2bc70900
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-02-01 18:11:13 +0100

    MATCHMAKIN HECHO (coña)

--- JuegoOcaFront/src/app/pages/matchmaking/matchmaking.component.ts
+++ JuegoOcaFront/src/app/pages/matchmaking/matchmaking.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-matchmaking',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './matchmaking.component.html',
   styleUrl: './matchmaking.component.css'
 })
