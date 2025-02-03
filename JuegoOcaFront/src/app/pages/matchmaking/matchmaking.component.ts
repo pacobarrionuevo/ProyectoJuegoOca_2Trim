7a29b6b9339366854127719dea9349307c2f0ebf
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-02-03 20:31:38 +0100

    MatchMaking bonito

--- JuegoOcaFront/src/app/pages/matchmaking/matchmaking.component.ts
+++ JuegoOcaFront/src/app/pages/matchmaking/matchmaking.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-matchmaking',
@@ -9,5 +10,13 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './matchmaking.component.css'
 })
 export class MatchmakingComponent {
-
+  tablero: string;
+  ROB: string;
+  NinosJugandoALaOca : string;
+  constructor(private imageService: ImageService) {
+      this.tablero = this.imageService.getImageUrl('TableroJuego.png');
+      this.ROB = this.imageService.getImageUrl('ROB.jpg')
+      this.NinosJugandoALaOca = this.imageService.getImageUrl('NiñosJugandoALaOca.jpg')
+    }
+  
 }
