91ca0fd08ebe1da9d38e68806f299375cad1a966
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-02-13 11:25:06 +0100

    si

--- JuegoOcaBack/Controllers/FriendRequestController.cs
+++ JuegoOcaBack/Controllers/FriendRequestController.cs
@@ -37,7 +37,7 @@ namespace JuegoOcaBack.Controllers
 
             var result = await _friendRequestService.SendFriendRequest(senderId, receiverId);
 
-            if (result) 
+            if (result)     
             {
                 return Ok();
             }
